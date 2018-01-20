4c3591f8e596961b543d236a6784d49f200bccee
TheArcaneBrony <myrainbowdash949@gmail.com>
2018-01-21 00:52:46 +0100

    reworking terrain gen

--- MCClone/MainWindow.cs
+++ MCClone/MainWindow.cs
@@ -18,11 +18,28 @@ namespace MCClone
         string progress = "";
         int renderDistance = 128;
         double brightness = 1 / 1;
-        Dictionary<Tuple<double, double>, Block> blockStorage = new Dictionary<Tuple<double, double>, Block>();
-        double[,] ceil = new double[100, 100];
+        static Dictionary<Tuple<double, double>, Block> blockStorage = new Dictionary<Tuple<double, double>, Block>();
         bool focussed = true;
         int centerX, centerY;
         double cxv = 0, cyv = 0, czv = 0;
+
+        public static void GenerateLine(Dictionary<Tuple<double, double>, Block> blockStorage, double xt)
+        {
+            for (double z = -100; z < 100; z += 1)
+            {
+                var blockPos = new Tuple<double, double>(xt, z);
+                if (!blockStorage.ContainsKey(blockPos)) blockStorage.Add(blockPos, new Block());
+                blockStorage[blockPos].Y = (int)(Math.Sin(xt / 20) + Math.Sin(z / 10)) % 10;
+
+                //Console.WriteLine((xt + "").PadLeft(6) + " | " + (z + "").PadLeft(6));
+                //Thread.Sleep(1);
+                // Thread.Sleep(new TimeSpan(15000));
+                //   progress = " | " + ((xt + "").PadLeft(6) + " | " + (z + "").PadLeft(6));
+                // Console.Title = blockStorage.Count + progress;
+            }
+            //Thread.Sleep(15);
+        }
+
         public MainWindow()
     : base(1280, // initial width
         720, // initial height
@@ -42,50 +59,48 @@ namespace MCClone
             GL.Viewport(ClientRectangle.X, ClientRectangle.Y, ClientRectangle.Width, ClientRectangle.Height);
             centerX = ClientRectangle.Width / 2;
             centerY = ClientRectangle.Height / 2;
-            Matrix4 projection = Matrix4.CreatePerspectiveFieldOfView(/*(float)Math.PI / 4*/ 0.7f, Width / (float)Height, 0.1f, 6400f);
+            Matrix4 projection = Matrix4.CreatePerspectiveFieldOfView(/*(float)Math.PI / 4*/ 0.9f, Width / (float)Height, 1.0f, 6400f);
             GL.MatrixMode(MatrixMode.Projection);
             GL.LoadMatrix(ref projection);
         }
+        [MTAThread]
         protected async override void OnLoad(EventArgs e)
         {
             CursorVisible = false;
-            
             GL.Enable(EnableCap.DepthTest);
             centerX = ClientRectangle.Width / 2;
             centerY = ClientRectangle.Height / 2;
             Mouse.Move += Mouse_Move;
-
-
-            Task.Run(async () =>
+            Thread worldGen = new Thread(new ThreadStart(() =>
             {
-                for (double x = -50; x < 50; x += 1)
+                for (double x = -100; x < 100; x += 1)
                 {
                     Console.Title = x + "";
-                    var xt = x;
-                    for (double z = -100; z < 100; z += 1)
-                    {
-                        var blockPos = new Tuple<double, double>(xt, z);
-                        if (!blockStorage.ContainsKey(blockPos)) blockStorage.Add(blockPos, new Block());
-                        blockStorage[blockPos].Y = x - (z*1.2);
-                        //Console.WriteLine((xt + "").PadLeft(6) + " | " + (z + "").PadLeft(6));
-                      //  Thread.Sleep(5);
-                       progress = " | " + ((xt + "").PadLeft(6) + " | " + (z + "").PadLeft(6)); 
-                    }
-                        //Thread.Sleep(15);
-                    }
-                progress = "";
-            });
+                    Thread childThread = new Thread(new ThreadStart(() => GenerateLine(blockStorage, x)));
+                    childThread.Start();
+                    childThread.Join();
+                }
+            }));
+            worldGen.Start();
+
+            progress = "";
             Thread.Sleep(1);
         }
         protected override void OnUpdateFrame(FrameEventArgs e)
         {
-            cyv -= 0.005;
-            if (cyv < -0.5) cyv = -0.45;
-            var pos = new Tuple<double, double>((int)cx, (int)cz);
-            if (blockStorage.ContainsKey(pos) && blockStorage[pos].Y > (cy - 2.1) && cyv < 0) cyv = 0;
-            cy += cyv;
-            HandleKeyboard();
+
+            Task.Run(async () =>
+            {
+                cyv -= 0.005;
+                if (cyv < -0.5) cyv = -0.45;
+                var pos = new Tuple<double, double>((int)cx, (int)cz);
+                if (blockStorage.ContainsKey(pos) && blockStorage[pos].Y > (cy - 2.1) && cyv < 0) cyv = 0;
+                cy += cyv;
+                HandleKeyboard();
+
+            });
             GL.ClearColor(0.1f * (float)brightness, 0.5f * (float)brightness, 0.7f * (float)brightness, 0.5f);
+
         }
         private void HandleKeyboard()
         {
@@ -132,40 +147,42 @@ namespace MCClone
                 OpenTK.Input.Mouse.SetPosition(mousePos.X, mousePos.Y);
                 clx += x;
                 cly += y;
-                if (cly > 480) cly = 480;
-                if (cly < -480) cly = -480;
-
+                if (cly > 512) cly = 512;
+                if (cly < -512) cly = -512;
+                //clx %= 360;
             }
         }
-        double cx = 0, cy = 0, cz = 0, clx = 0.5, cly = 0, clz = 0.5;
+        double cx = 0, cy = 100, cz = 0, clx = 0, cly = 0;
         protected override void OnRenderFrame(FrameEventArgs e)
         {
             Title = $"(Vsync: {VSync}) FPS: {1f / e.Time:0} | {cx}/{cy}/{cz} | {clx}/{cly} {progress}";
             GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
-            Matrix4 modelview = Matrix4.LookAt(new Vector3((float)cx, (float)cy + 2, (float)cz), new Vector3((float)(cx + Math.Cos((clx / 360)) * 360), (float)(cy + 2 + Math.Sin((cly / 360)) * 480), (float)(cz + Math.Sin((clx / 360)) * 360)), Vector3.UnitY);
+            Matrix4 modelview = Matrix4.LookAt(new Vector3((float)cx, (float)cy + 2, (float)cz), new Vector3((float)(cx + Math.Cos((clx / 360)) * 360), (float)(cy + 2 + Math.Sin((cly / 360)) * 480) * 2, (float)(cz + Math.Sin((clx / 360)) * 360)), Vector3.UnitY);
             GL.MatrixMode(MatrixMode.Modelview);
             GL.LoadMatrix(ref modelview);
-            for (int x = (int)cx - renderDistance; x < (int)cx + renderDistance; x++)
+            Thread blockRender = new Thread(new ThreadStart(() =>
             {
-                for (int z = (int)cz - renderDistance; z < (int)cz + renderDistance; z++)
+                for (int x = (int)cx - renderDistance; x < (int)cx + renderDistance; x++)
                 {
-                    var blockPos = new Tuple<double, double>((int)x, (int)z);
-                    if (blockStorage.ContainsKey(blockPos))
-                        renderCube(x, blockStorage[blockPos].Y, z);
+                    for (int z = (int)cz - renderDistance; z < (int)cz + renderDistance; z++)
+                    {
+                        var blockPos = new Tuple<double, double>((int)x, (int)z);
+                        if (blockStorage.ContainsKey(blockPos))
+                            renderCube(x, blockStorage[blockPos].Y, z);
+                    }
                 }
-            }
+            }));
+            blockRender.Start();
+            
 
             GL.Begin(BeginMode.Points);
             GL.Color3(1f, 1f, 1f);
-            GL.Vertex3(clx, cly, clz);
+            GL.Vertex3((float)(cx + Math.Cos((clx / 360)) * 360), (float)(cy + 2 + Math.Sin((cly / 360)) * 480) * 2, (float)(cz + Math.Sin((clx / 360)) * 360));
             GL.End();
             SwapBuffers();
         }
         void renderCube(double x, double y, double z)
         {
-            var blockPos = new Tuple<double, double>(x, z);
-            if (!blockStorage.ContainsKey(blockPos)) blockStorage.Add(blockPos, new Block());
-            blockStorage[blockPos].Y = y;
             GL.Begin(BeginMode.Quads);
             //top
             GL.Color3(1.0f * brightness, 1.0f * brightness, 0.0f * brightness);
--- MCClone/Program.cs
+++ MCClone/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using OpenTK;
 using OpenTK.Graphics;
@@ -9,13 +10,18 @@ using OpenTK.Platform;
 
 namespace MCClone
 {
+   
     class Program
     {
+        [MTAThread]
         static void Main(string[] args)
         {
             Console.WriteLine("Controls:\nW: X+\nS: X-\nD: Z+\nA: Z-\nQ: Brightness down\nE: Brightness up\nSpace: Jump/fly\nShift: Descend\nR: Go to 0/100/0");
-            Console.ReadKey();
-            Console.Write("\b ");
+            for (int i = 30; i > 0; i--)
+            {
+                Thread.Sleep(50);
+                Console.Write("\r" + new string(' ',i+5) +"\r"+new string('.',i));
+            }
             new MainWindow().Run(120);
         }
     }
