64187eb4e40ca96298236d105b0cb924b65b4a2e
TheArcaneBrony <myrainbowdash949@gmail.com>
2018-11-19 16:03:41 +0100

    Trying to add shaders..

--- MCClone/Logger.cs
+++ MCClone/Logger.cs
@@ -1,48 +1,35 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
-using System.Linq;
 using System.Net;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace MCClone
 {
     class Logger
     {
+        public static List<string> LogQueue = new List<string>();
         public static void PostLog(string Log)
-        {
-            // Create a request using a URL that can receive a post.   
+        {/*
             WebRequest request = WebRequest.Create("http://thearcanebrony.ddns.net/PushMCCloneLog.php");
-            // Set the Method property of the request to POST.  
             request.Method = "POST";
-            // Create POST data and convert it to a byte array.  
             string postData = Log;
             byte[] byteArray = Encoding.UTF8.GetBytes(postData);
-            // Set the ContentLength property of the WebRequest.  
             request.ContentLength = byteArray.Length;
-            // Get the request stream.  
             Stream dataStream = request.GetRequestStream();
-            // Write the data to the request stream.  
             dataStream.Write(byteArray, 0, byteArray.Length);
-            // Close the Stream object.  
             dataStream.Close();
-            // Get the response.  
             WebResponse response = request.GetResponse();
-           /* // Display the status.  
-            Console.WriteLine(((HttpWebResponse)response).StatusDescription);*/
-            // Get the stream containing content returned by the server.  
             dataStream = response.GetResponseStream();
-            // Open the stream using a StreamReader for easy access.  
             StreamReader reader = new StreamReader(dataStream);
-            // Read the content.  
             string responseFromServer = reader.ReadToEnd();
-            // Display the content.  
-            Console.Write(responseFromServer);
-            // Clean up the streams.  
+            //Console.Write(responseFromServer);
             reader.Close();
             dataStream.Close();
-            response.Close();
+            response.Close();*/
+
+            Debug.WriteLine(Log);
         }
     }
 }
--- MCClone/Player.cs
+++ MCClone/Player.cs
@@ -15,14 +15,21 @@ namespace MCClone
         public double X { get; set; }
         public double Y { get; set; }
         public double Z { get; set; }
-        public int CX { get { return (int)this.X/16; } }
-        public int CZ { get { return (int)this.Z / 16; } }
+        public int Xa { get; set; }
+        public int Ya { get; set; }
+        public int Za { get; set; }
+        public float Xo { get; set; }
+        public float Yo { get; set; }
+        public float Zo { get; set; }
+        public int Xc { get { return (int)this.Xa/16; } }
+        public int Zc { get { return (int)this.Za / 16; } }
         public double XV { get; set; }
         public double YV { get; set; }
         public double ZV { get; set; }
         public double LX { get; set; }
         public double LY { get; set; }
         public bool Flying { get; set; }
+        public bool InAir { get; set; }
 
         public Vector3 CPos { get; set; }
         public Vector3 CFPt { get; set; }
