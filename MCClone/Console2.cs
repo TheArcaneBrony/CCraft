85723762ed0d6d7492458a53c8163c49db05965c
TheArcaneBrony <myrainbowdash949@gmail.com>
2019-02-18 00:06:38 +0100

    Added second console (never commited local change)

---
+++ MCClone/Console2.cs
@@ -0,0 +1,30 @@
+﻿
+using System.Diagnostics;
+using System.IO;
+
+namespace MCClone
+{
+    class Console2
+    {
+        public static void Init()
+        {
+            ProcessStartInfo psi = new ProcessStartInfo("cmd.exe")
+            {
+                RedirectStandardError = true,
+                RedirectStandardInput = true,
+                RedirectStandardOutput = true,
+                UseShellExecute = false,
+                CreateNoWindow = false,
+                Arguments = "/k @echo off",
+                WindowStyle = ProcessWindowStyle.Normal
+            };
+
+            Process p = Process.Start(psi);
+            StreamWriter sw = p.StandardInput;
+            StreamReader sr = p.StandardOutput;
+
+            sw.WriteLine("Hello world!");
+            sr.Close();
+        }
+    }
+}
